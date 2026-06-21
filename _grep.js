058c206314eb2fd4fb6ab1a465393361594124e7
ohyeswyd1994 <ohyeswyd1994@users.noreply.github.com>
2026-06-21 15:01:42 +0800

    update

--- _grep.js
+++ _grep.js
@@ -1,1 +1,10 @@
-// safe to delete
+const fs = require('fs');
+const html = fs.readFileSync('public/index.html', 'utf8');
+const m = html.match(/<script>([\s\S]*?)<\/script>/);
+try {
+  new Function(m[1]);
+  console.log('CLIENT_OK');
+} catch (e) {
+  console.error('CLIENT_ERROR:', e.message);
+  process.exit(1);
+}
